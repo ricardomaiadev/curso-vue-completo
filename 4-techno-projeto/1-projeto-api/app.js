a6c4718eca5641d1250583bb6590fbc41c4189d7
Ricardo <riicardolivera@gmail.com>
2021-06-22 17:58:37 -0400

    refactor(app): Acrescentado abrirModal, fecharModal, falta terminar adicionarItem

--- 4-techno-projeto/1-projeto-api/app.js
+++ 4-techno-projeto/1-projeto-api/app.js
@@ -1,32 +1,48 @@
 const vm = new Vue({
-    el:"#app",
+    el: "#app",
     data: {
         produtos: [],
-        produto: false
+        produto: false,
+        carrinho: [],
+        carrinhoTotal: 0,
     },
     filters: {
-        numeroPreco(valor){
-            return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
         }
     },
 
     methods: {
         fetchProdutos() {
             fetch("./api/produtos.json")
-            .then(r => r.json())
-            .then(r => {
-                this.produtos = r;
-            })          
+                .then(r => r.json())
+                .then(r => {
+                    this.produtos = r;
+                })
         },
-        fetchProduto(id){
+        fetchProduto(id) {
             fetch(`./api/produtos/${id}/dados.json`)
-            .then(r => r.json())
-            .then(r => {
-                this.produto = r;
+                .then(r => r.json())
+                .then(r => {
+                    this.produto = r;
+                })
+        },
+        abrirModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
             })
-        }
+        },
+        fecharModal({ target, currentTarget }) {
+            if (target === currentTarget) this.produto = false;
+
+        },
+        adicionarItem(event) {
+            this.produto.estoque--;
+        },
     },
-    created(){
+    created() {
         this.fetchProdutos();
     }
 })
