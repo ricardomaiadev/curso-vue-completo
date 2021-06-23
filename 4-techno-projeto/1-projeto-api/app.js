736e78c539ff1d77d6041df1da4cffdcb03a8fc4
Ricardo <riicardolivera@gmail.com>
2021-06-23 18:02:45 -0400

    refactor(app): Add item, removerItem, msgAlert e localStorage.

--- 4-techno-projeto/1-projeto-api/app.js
+++ 4-techno-projeto/1-projeto-api/app.js
@@ -4,11 +4,24 @@ const vm = new Vue({
         produtos: [],
         produto: false,
         carrinho: [],
-        carrinhoTotal: 0,
+        mensagemAlerta: "Item adicionado",
+        alertaAtivo: false,
     },
     filters: {
         numeroPreco(valor) {
             return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+        }
+    },
+
+    computed: {
+        carrinhoTotal() {
+            let total = 0;
+            if (this.carrinho.length) {
+                this.carrinho.forEach(item => {
+                    total += item.preco;
+                })
+            }
+            return total;
         }
     },
 
@@ -38,11 +51,34 @@ const vm = new Vue({
             if (target === currentTarget) this.produto = false;
 
         },
-        adicionarItem(event) {
+        adicionarItem() {
             this.produto.estoque--;
+            const { id, nome, preco } = this.produto;
+            this.carrinho.push({ id, nome, preco });
+            this.alerta(`${nome} adicionado ao carrinho.`);
         },
+        removerItem(index) {
+            this.carrinho.splice(index, 1);
+        },
+        checarLocalStorage() {
+            if (window.localStorage.carrinho)
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+        },
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false;
+            }, 1500);
+        },
+    },
+    watch: {
+        carrinho() {
+            window.localStorage.carrinho = JSON.stringify(this.carrinho);
+        }
     },
     created() {
         this.fetchProdutos();
+        this.checarLocalStorage();
     }
 })
