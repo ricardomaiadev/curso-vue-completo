6416fa4e8a4c9418f9d0ee8a35d072bcdda619fe
Ricardo <riicardolivera@gmail.com>
2021-06-25 15:17:51 -0400

    refactor(app): Foi adicionado a função comparar estoque, pra não dar erro no controle de estoque do produto.

--- 4-techno-projeto/1-projeto-api/app.js
+++ 4-techno-projeto/1-projeto-api/app.js
@@ -69,6 +69,10 @@ const vm = new Vue({
             if (window.localStorage.carrinho)
                 this.carrinho = JSON.parse(window.localStorage.carrinho);
         },
+        compararEstoque() {
+            const items = this.carrinho.filter(({ id }) => id === this.produto.id)
+            this.produto.estoque = this.produto.estoque - items.length;
+        },
         alerta(mensagem) {
             this.mensagemAlerta = mensagem;
             this.alertaAtivo = true;
@@ -86,7 +90,10 @@ const vm = new Vue({
         produto() {
             document.title = this.produto.nome || "Techno";
             const hash = this.produto.id || "";
-            history.pushState(null, null, `#${hash}`)
+            history.pushState(null, null, `#${hash}`);
+            if (this.produto) {
+                this.compararEstoque();
+            }
         },
         carrinho() {
             window.localStorage.carrinho = JSON.stringify(this.carrinho);
