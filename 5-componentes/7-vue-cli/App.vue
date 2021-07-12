3b86b199680a789d2adb3d7191f5172745dae796
Ricardo <riicardolivera@gmail.com>
2021-07-12 17:44:36 -0400

    add(vue): explicação do vue.

---
+++ 5-componentes/7-vue-cli/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <P>{{ mensagem }}</P>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      mensagem: 'Verificando se funciona',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
