113e1ec73612c8ae2a95a30b9482e5cb7d5f4327
Ricardo <riicardolivera@gmail.com>
2021-07-13 17:55:11 -0400

    add(slots): Parado faltando 5min.

---
+++ 5-componentes/9-slots/vue-slots/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <modal-principal>
+      <template #header>
+        <p>Esse é o header</p>
+      </template>
+      <template #default>
+        <p>Esse é um teste</p>
+        <button>Comprar</button>
+      </template>
+      <template #footer>
+        <p>Esse é o footer</p>
+      </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from './components/ModalPrincipal.vue';
+export default {
+  components: {
+    ModalPrincipal,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 5-componentes/9-slots/vue-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="modal">
+    <slot name="header"></slot>
+    <h1>Esse é o modal</h1>
+    <slot></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ModalPrincipal',
+};
+</script>
+<style>
+</style>
