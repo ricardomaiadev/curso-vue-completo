e9f6ce8b7f09c46c09840ffbbb42bc0eb74f9517
Ricardo <rick.maia@ymail.com>
2021-07-16 17:50:16 -0400

    add(async): Aula finalizada.

---
+++ 5-componentes/11-async/vue-dynamic/src/components/ContatoEmpresa.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Contato Empresa</h1>
+        <input type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ContatoEmpresa"
+}
+</script>
+
+<style>
+
+</style>
---
+++ 5-componentes/11-async/vue-dynamic/src/components/ServicoEmpresa.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Serviços</h1>
+        <ul>
+            <li>UX/UI Designeer</li>
+            <li>Front end</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ServicoEmpresa"
+}
+</script>
+
+<style>
+
+</style>
