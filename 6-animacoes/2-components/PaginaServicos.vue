7fe095a1789e14e741699b02af6110aea83cfa81
Ricardo <rick.maia@ymail.com>
2021-07-16 17:52:17 -0400

    add(animacao): Aula finalizada de components.

---
+++ 6-animacoes/2-components/PaginaServicos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Serviços da Empresa</h1>
+        <ul>
+            <li>Front End</li>
+            <li>UX/UI Design</li>
+            <li>Marketing</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
