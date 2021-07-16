31e801bdef125b805b3ecd56c9783cec5eb9e18a
Ricardo <rick.maia@ymail.com>
2021-07-16 17:53:00 -0400

    add(animacao): Aula de list, falta finalizar.

---
+++ 6-animacoes/3-list/list.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            carrinho: [
+                {
+                    nome: "Notebook",
+                    preço: 2399
+                },
+                {
+                    nome: "Smartphone",
+                    preço: 1399
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
