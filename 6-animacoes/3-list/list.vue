6b6fdbc2ea9f63ad2cac39f5a315fbe4e8ae9662
Ricardo <rick.maia@ymail.com>
2021-07-19 17:52:17 -0400

    refactor(list): finalizado aula de animacao de lista.

--- 6-animacoes/3-list/list.vue
+++ 6-animacoes/3-list/list.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <ul>
-            <li></li>
-        </ul>
+        <transition-group tag="ul">
+            <li v-for="(produto, index) in carrinho" :key="produto.nome">
+                <p>{{produto.nome}}</p>
+                <p>{{produto.preco}}</p>
+                <button @click="remover(index)">Remover</button>
+            </li>
+        </transition-group>
     </div>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
@@ -21,10 +26,26 @@ export default {
                 }
             ]
         }
+    },
+    methods: {
+        remover(index) {
+            this.carrinho.splice(index, 1);
+        }
     }
 }
 </script>
 
 <style>
+
+.v-enter-active,
+.v-leave-active {
+    transition: all 0.3s;
+}
+
+.v-enter,
+.v-leave-to {
+    opacity: 0;
+    transform: translate3d(-20px, 0, 0);
+}
 
 </style>
