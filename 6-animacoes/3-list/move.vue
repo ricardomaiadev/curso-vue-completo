53f55c8aef0efed112fc9b089c58248502ecff7d
Ricardo <rick.maia@ymail.com>
2021-07-19 17:52:37 -0400

    refactor(list): finalizado aula de animacao de lista com move.

---
+++ 6-animacoes/3-list/move.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <button @click="filtroAZ(index)">A - Z</button>
+        <button @click="filtroZA(index)">Z - A</button>
+        <transition-group tag="ul">
+            <li v-for="filme in filmes" :key="filme">
+                <p>{{filme}}</p>
+            </li>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            filmes: [ "O Senhor dos Aneis" , "As Crônicas de Nárnia", "Coração Valente", "Harry Potter" ]
+        }
+    },
+    methods: {
+        filtroAZ() {
+            this.filmes.sort();
+        },
+        filtroZA() {
+            this.filmes.sort().reverse();
+        }
+    }
+}
+</script>
+
+<style>
+
+.v-move {
+    transition: transform .3s;
+}
+
+li {
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,.1);
+    background: white;
+    padding: 20px;
+    max-width: 200px;
+    margin-bottom: 10px;
+}
+
+</style>
