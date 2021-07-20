630384515ff80ac109338ef72d2acfdc5b8e60b9
Ricardo <rick.maia@ymail.com>
2021-07-20 12:23:19 -0400

    add(router): Iniciado aula de routas no vue.

---
+++ 7-vue-router/1-router/app-router/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Logo</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/Cursos">Cursos</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a {
+  padding: 10px;
+}
+
+.router-link-exact-active {
+  color: #84e;
+  font-weight: bold;
+}
+</style>
---
+++ 7-vue-router/1-router/app-router/src/views/Cursos.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Essa é a pagina Cursos</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
