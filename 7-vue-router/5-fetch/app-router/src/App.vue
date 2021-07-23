cc9ce733a60630fcf216ed3eb990c6ccf6d7f421
Ricardo <riicardolivera@gmail.com>
2021-07-23 16:38:42 -0400

    add(fetch): Iniciado aula sobre fetch.

---
+++ 7-vue-router/5-fetch/app-router/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1 @click="mostrarRouter">Logo</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/cursos">Cursos</router-link>
+      <router-link to="/Acoes">Açōes</router-link>
+    </nav>
+    <transition mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+export default {
+    methods: {
+      mostrarRouter() {
+        console.log(this.$route);
+      }
+    }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #234;
+  border: 4px solid;
+  margin: 20px;
+  padding: 20px;
+}
+
+nav {
+  padding: 20px 0;
+}
+
+a {
+  padding: 10px;
+  color: #234;
+}
+
+.router-link-exact-active {
+  color: #4b8;
+  font-weight: bold;
+}
+
+.v-enter,
+.v-leave-to {
+  transform: translate3d(-20px, 0, 0);
+  opacity: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
+}
+</style>
