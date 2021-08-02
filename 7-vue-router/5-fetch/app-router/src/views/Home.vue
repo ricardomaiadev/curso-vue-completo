d4a648fe41808f8b7fd86286720e0dda6d0c3a44
Ricardo <riicardolivera@gmail.com>
2021-08-02 17:55:49 -0400

    refactor(home): add set timeout para acoes.

--- 7-vue-router/5-fetch/app-router/src/views/Home.vue
+++ 7-vue-router/5-fetch/app-router/src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
-    <h1>Essa é a Home</h1>
+    <h1 @click="navegarCursos">Essa é a Home</h1>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        navegarCursos() {
+            setTimeout(() =>{
+                this.$router.push("acoes");
+            }, 1000)
+        }
+    }
 }
 </script>
 
