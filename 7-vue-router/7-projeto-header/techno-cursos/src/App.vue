c62c5f0d135e2456bf065157175cb4dffedd849e
Ricardo <riicardolivera@gmail.com>
2021-08-03 12:59:45 -0400

    add(proj-header): Conclusao da aula.

---
+++ 7-vue-router/7-projeto-header/techno-cursos/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <TheHeader/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: { TheHeader },
+
+}
+</script>
+
+
+<style>
+
+body {
+    margin: 0px;
+    font-family: Helvetica, Arial, sans-serif;
+    color: #234;
+}
+ul {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+}
+
+a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #234;
+}
+
+a.router-link-active {
+    color: #4b8;
+}
+
+</style>
---
+++ 7-vue-router/7-projeto-header/techno-cursos/src/views/Contato.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Contato</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contato',
+}
+</script>
