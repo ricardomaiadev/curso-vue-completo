c4f82867a9019fde282fe52c68ba029a08f4452a
Ricardo <riicardolivera@gmail.com>
2021-08-03 17:58:12 -0400

    add(contato): Finalizado aula de contato.

---
+++ 7-vue-router/11-contato/techno-cursos/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <TheHeader/>
+    <router-view class="container"></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: { TheHeader },
+
+}
+</script>
+
+
+<style>
+
+body {
+    margin: 0px;
+    font-family: Helvetica, Arial, sans-serif;
+    color: #234;
+}
+ul {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+}
+
+img {
+  max-width: 100%;
+}
+
+a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #234;
+}
+
+a.router-link-active {
+    color: #4b8;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all .3s;
+}
+
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  grid-gap: 30px;
+}
+
+@media screen and (max-width: 400px) {
+  
+  .conteudo {
+    display: block;
+  }
+}
+
+</style>
---
+++ 7-vue-router/11-contato/techno-cursos/src/views/Cursos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading/>
+    </div>
+    <transition>
+      <div v-if="api">
+        <h1>Cursos</h1>
+        <p>{{api}}</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+
+export default {
+  name: 'cursos',
+    mixins: [fetchData],
+      created() {
+    this.fetchData("/cursos");
+  }
+}
+</script>
