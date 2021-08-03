2c708244fa60f664209a65fe9a3bc267b26fc4a7
Ricardo <riicardolivera@gmail.com>
2021-08-03 16:23:23 -0400

    add(mixins): Conclusão da aula.

---
+++ 7-vue-router/8-mixins/techno-cursos/src/views/Contato.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <p>Carregando...</p>
+    </div>
+    <div v-if="api">
+      <h1>Contato</h1>
+      <p>{{api}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+
+export default {
+  name: 'contato',
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/contato");
+  }
+}
+</script>
