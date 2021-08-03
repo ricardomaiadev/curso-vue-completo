80afecaf89537015e08a17ccaced7afed2af4f9c
Ricardo <riicardolivera@gmail.com>
2021-08-03 12:34:44 -0400

    add(json): Conclusao da aula de json server

---
+++ 7-vue-router/6-json-server/techno-cursos/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TheHeader/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: { TheHeader },
+
+}
+</script>
+
+
+<style>
+</style>
---
+++ 7-vue-router/6-json-server/techno-cursos/src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav>
+            <div class="logo">
+                <router-link to="/">Techno</router-link>
+            </div>
+            <ul>
+                <li>
+                    <router-link to="/cursos">Cursos</router-link>
+                </li>
+                <li>
+                    <router-link to="/contato">Contato</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
