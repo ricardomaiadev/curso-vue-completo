12243ffbce8f6510fac9874e39ff75a761ab7b08
Ricardo <riicardolivera@gmail.com>
2021-08-03 17:51:10 -0400

    add(home): Finalizado a home. concluido a aula.

---
+++ 7-vue-router/10-home/techno-cursos/src/views/Contato.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading/>
+    </div>
+    <transition>
+      <div v-if="api">
+        <h1>Contato</h1>
+        <p>{{api}}</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+
+export default {
+  name: 'contato',
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/contato");
+  }
+}
+</script>
