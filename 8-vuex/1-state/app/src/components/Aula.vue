ee8982ecf8d31fe51b1d3eaf255cc0b46e81b6b4
Ricardo <riicardolivera@gmail.com>
2021-08-04 17:52:37 -0400

    add(state): Finalizado aula sobre state.

---
+++ 8-vuex/1-state/app/src/components/Aula.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>Aula {{totalAulas}}, esse é o usuário: {{user}} , {{aulasCompletas}} </p>
+        <button @click="mostrarConsole"> Mostrar no Console</button>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+    data() {
+        return {
+            totalAulas: 25,
+            nome: "Ricardo",
+        }
+    },
+    computed: {
+        ...mapState(["user", "aulasCompletas"]),
+        nomeMaisculo() {
+            return this.nome.toUpperCase();
+        }
+    },
+    methods: {
+        handleClick() {
+            console.log(this.user)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
