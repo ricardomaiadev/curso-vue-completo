2192565e55deca406c901cc781099d8d9d68aaca
souricardomaia <riicardolivera@gmail.com>
2021-08-03 22:21:36 -0400

    add(aula): Falta finalizar, verificar o erro de não está mostrando a view.

---
+++ 7-vue-router/13-aula/techno-cursos/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Contato from '../views/Contato.vue'
+import Cursos from '../views/Cursos.vue'
+import Curso from '../views/Curso.vue'
+import Aula from '../views/Aula.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/contato",
+    name: "contato",
+    component: Contato,
+  },
+  {
+    path: "/cursos",
+    name: "cursos",
+    component: Cursos,
+  },
+  {
+    path: "/cursos/:curso",
+    name: "curso",
+    component: Curso,
+    props: true,
+    children: [
+      {
+        path: ":aula",
+        name: "aula",
+        component: Aula,
+        props: true,
+      }
+    ]
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ 7-vue-router/13-aula/techno-cursos/src/views/Aula.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ Aula }}</p>
+</template>
+
+<script>
+import fetchData from '@/mixins/fetchData.js';
+
+export default {
+  name: 'aula',
+  props: ['aula'],
+  mixins: [fetchData],
+  created() {
+    this.fetchData(`/curso/${this.curso}`);
+  },
+};
+</script>
+
+<style></style>
