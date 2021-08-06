f8a69db77828a95e5faf2580fde50efb917a0b4c
Ricardo <riicardolivera@gmail.com>
2021-08-06 18:01:16 -0400

    add(filter): Aula finalizada.

---
+++ 9-projeto-final/12-produto-filter/ranek/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Produto from '../views/Produto.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/produto/:id',
+    name: 'produto',
+    component: Produto,
+    props: true
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior() {
+    return window.scrollTo({top: 0, behavior: "smooth"});
+  }
+})
+
+export default router
+
