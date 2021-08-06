04b81d79e613f504e7bc16b758471a80444a88e2
Ricardo <riicardolivera@gmail.com>
2021-08-06 18:01:06 -0400

    add(transicao): Aula finalizada.

---
+++ 9-projeto-final/11-transicao/ranek/src/components/ProdutosListar.vue
@@ -0,0 +1,115 @@
+<template>
+  <section class="produtos-container" >
+    <transition mode="out-in">
+      <div v-if="produtos && produtos.length > 0" class="produtos" key="produtos">
+          <div class="produto" v-for="(produto, index) in produtos" :key="index">
+            <router-link to="/">
+              <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+              <p class="preco">{{produto.preco}}</p>
+              <h2 class="titulo">{{produto.nome}}</h2>
+              <p>{{produto.descricao}}</p>
+            </router-link>
+          </div>
+        <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina ="produtosPorPagina" />
+      </div>
+      <div v-else-if="produtos && produtos.length === 0" key="sem-resultados">
+        <p class="sem-resultados" >Busca sem resultados. Tente buscar outro termo.</p>
+      </div>
+      <PaginaCarregando key="carregando" v-else/>
+    </transition>
+  </section>
+</template>
+
+<script>
+import ProdutosPaginar from "@/components/ProdutosPaginar.vue";
+import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
+
+export default {
+  name: 'ProdutoListar',
+  components: {
+    ProdutosPaginar,
+  },
+  data() {
+    return {
+      produtos: null,
+      produtosPorPagina: 9,
+      produtosTotal: 0,
+    };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    }
+  },
+  methods: {
+    getProdutos() {
+      this.produtos = null;
+      window.setTimeout(() => {
+        api.get(this.url).then(r => {
+          this.produtosTotal = Number(r.headers["x-total-count"]);
+          this.produtos = r.data;
+          });
+        }, 1500);
+      },
+    },
+  watch: {
+    url() {
+      this.getProdutos();
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
+};
+</script>
+
+
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 36px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
+
+</style>
