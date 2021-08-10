9ab72bd08186b11dbae5ee7318eec9d040e9ee9e
Ricardo <riicardolivera@gmail.com>
2021-08-10 17:57:14 -0400

    add(cep-api): Aula concluida

---
+++ 9-projeto-final/17-cep-api/ranek/src/router.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Produto from "./views/Produto.vue";
+import Login from "./views/Login.vue";
+import Usuario from "./views/usuario/Usuario.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/produto/:id",
+      name: "produto",
+      component: Produto,
+      props: true
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/usuario",
+      name: "usuario",
+      component: Usuario
+    }
+  ],
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+});
---
+++ 9-projeto-final/17-cep-api/ranek/src/store.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { api } from "@/services.js";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: true,
+  state: {
+    login: false,
+    usuario: {
+      id: "",
+      nome: "",
+      email: "",
+      senha: "",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: ""
+    }
+  },
+  mutations: {
+    UPDATE_LOGIN(state, payload) {
+      state.login = payload;
+    },
+    UPDATE_USUARIO(state, payload) {
+      state.usuario = Object.assign(state.usuario, payload);
+    }
+  },
+  actions: {
+    getUsuario(context, payload) {
+      api.get(`/usuario/${payload}`).then(response => {
+        context.commit("UPDATE_USUARIO", response.data);
+        context.commit("UPDATE_LOGIN", true);
+      });
+    }
+  }
+});
