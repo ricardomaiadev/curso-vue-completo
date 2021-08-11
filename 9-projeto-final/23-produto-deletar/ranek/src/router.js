8fcde6a5b7c44a274cfcf908a85e5cdfe72c37a0
Ricardo <riicardolivera@gmail.com>
2021-08-11 10:09:44 -0400

    add(produto-deletar): Aula concluida

---
+++ 9-projeto-final/23-produto-deletar/ranek/src/router.js
@@ -0,0 +1,64 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Produto from "./views/Produto.vue";
+import Login from "./views/Login.vue";
+import Usuario from "./views/usuario/Usuario.vue";
+import UsuarioProdutos from "./views/usuario/UsuarioProdutos.vue";
+import UsuarioVendas from "./views/usuario/UsuarioVendas.vue";
+import UsuarioCompras from "./views/usuario/UsuarioCompras.vue";
+import UsuarioEditar from "./views/usuario/UsuarioEditar.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/produto/:id",
+      name: "produto",
+      component: Produto,
+      props: true
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/usuario",
+      component: Usuario,
+      children: [
+        {
+          path: "",
+          name: "usuario",
+          component: UsuarioProdutos
+        },
+        {
+          path: "compras",
+          name: "compras",
+          component: UsuarioCompras
+        },
+        {
+          path: "vendas",
+          name: "vendas",
+          component: UsuarioVendas
+        },
+        {
+          path: "editar",
+          name: "usuario-editar",
+          component: UsuarioEditar
+        }
+      ]
+    }
+  ],
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+});
