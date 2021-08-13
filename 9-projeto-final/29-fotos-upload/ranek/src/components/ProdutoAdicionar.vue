c2f18b3af708ae07ebbafd5c81a6911172dc7046
Ricardo <riicardolivera@gmail.com>
2021-08-13 16:52:09 -0400

    add(Upload-Fotos): Aula concluida

---
+++ 9-projeto-final/29-fotos-upload/ranek/src/components/ProdutoAdicionar.vue
@@ -0,0 +1,73 @@
+<template>
+    <form class="adicionar-produto">
+        <label for="nome">Nome</label>
+        <input id="nome" name="nome" type="text" v-model="produto.nome">
+        <label for="preco">Preço</label>
+        <input id="preco" name="preco" type="number" v-model="produto.preco">
+        <label for="fotos">Fotos</label>
+        <input id="fotos" name="fotos" type="file" multiple ref="fotos">
+        <label for="descricao">Descricao</label>
+        <textarea id="descricao" name="descricao" v-model="produto.descricao"></textarea>
+        <input class="btn" type="button" value="Adicionar Produto" @click.prevent="adicionarProduto">
+    </form>
+</template>
+
+<script>
+import { api } from "@/services.js"
+
+export default {
+    name: "ProdutoAdicionar",
+    data() {
+        return {
+            produto: {
+                nome: "",
+                preco: "",
+                fotos: "",
+                descricao: "",
+                fotos: null,
+                vendido: "false",
+            }
+        }
+    },
+    methods: {
+        formatarProduto() {
+            const form = new FormData();
+
+            const files = this.$refs.fotos.files;
+            console.log(files);
+            for (let i = 0; i < files.length; i++){
+                form.append(files[i].name, files[i]);
+            }
+
+            form.append("nome", this.produto.nome);
+            form.append("preco", this.produto.preco);
+            form.append("descricao", this.produto.descricao);
+            form.append("vendido", this.produto.vendido);
+            form.append("usuario_id", this.$store.state.usuario.id);
+
+            return form;
+         },
+        adicionarProduto() {
+            const produto = this.formatarProduto();
+            api.post("/produto", produto).then(() => {
+                this.$store.dispatch("getUsuarioProdutos")
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.adicionar-produto {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    align-items: center;
+    margin-bottom: 60px;
+}
+
+.btn {
+    grid-column: 2;
+}
+
+</style>
---
+++ 9-projeto-final/29-fotos-upload/ranek/src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuario</button>
+    </UsuarioForm>
+  </section>
+</template>
+
+<script>
+import UsuarioForm from "@/components/UsuarioForm.vue"
+import { api } from "@/services.js";
+
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm
+  },
+  methods: {
+    atualizarUsuario() {
+       api.put("/usuario", this.$store.state.usuario)
+       .then(() => {
+         this.$store.dispatch("getUsuario");
+         this.$router.push({name: "usuario"});
+       }). catch(error => {
+         console.log(error.response)
+       });
+    }
+   
+  }
+};
+</script>
+
+<style>
+</style>
