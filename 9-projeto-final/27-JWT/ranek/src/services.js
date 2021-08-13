b61163bc67523ac956ed7aeadb1f0f7dae7f0673
Ricardo <riicardolivera@gmail.com>
2021-08-13 16:51:54 -0400

    add(JWT): Aula concluida

---
+++ 9-projeto-final/27-JWT/ranek/src/services.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost/ranekapi//wp-json/api/'
+});
+
+axiosInstance.interceptors.request.use(
+  function(config) {
+    const token = window.localStorage.token;
+    if(token) {
+      config.headers.Authorization = token
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+)
+
+export const api = {
+  get(endpoint) {
+    return axiosInstance.get(endpoint);
+  },
+  post(endpoint, body) {
+    return axiosInstance.post(endpoint, body);
+  },
+  put(endpoint, body) {
+    return axiosInstance.put(endpoint, body);
+  },
+  delete(endpoint) {
+    return axiosInstance.delete(endpoint);
+  },
+  login(body) {
+    return axios.post("http://localhost/ranekapi//wp-json/jwt-auth/v1/token", body);
+  },
+  validateToken() {
+    return axios.post("http://localhost/ranekapi//wp-json/jwt-auth/v1/token/validate");
+  },
+};
+
+export function getCep(cep) {
+  return axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+}
