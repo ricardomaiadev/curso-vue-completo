292e6e726d0ded6a38b3e43d3d5771067f80f5c1
Ricardo <riicardolivera@gmail.com>
2021-08-16 15:25:23 -0400

    add(title-e-mais): Aula concluida

---
+++ 9-projeto-final/35-title-e-mais/ranek/src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuario</button>
+    </UsuarioForm>
+    <ErroNotificacao :erros="erros"/>
+  </section>
+</template>
+
+<script>
+import UsuarioForm from "@/components/UsuarioForm.vue"
+import { api } from "@/services.js";
+
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm
+  },
+  data() {
+    return {
+      erros: []
+    }
+  },
+  methods: {
+    atualizarUsuario() {
+      this.erros = [];
+       api.put("/usuario", this.$store.state.usuario)
+       .then(() => {
+         this.$store.dispatch("getUsuario");
+         this.$router.push({name: "usuario"});
+       }). catch(error => {
+          this.erros.push(error.response.data.message);
+       });
+    }
+  },
+  created() {
+  document.title = "Usuário | Editar ";
+  }
+};
+</script>
+
+<style>
+</style>
