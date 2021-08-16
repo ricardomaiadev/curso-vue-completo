8402572a14280e42661a5af73e70d04ac907b113
Ricardo <riicardolivera@gmail.com>
2021-08-16 15:23:26 -0400

    add(estilo-responsivo): Aula concluida

---
+++ 9-projeto-final/32-estilo-responsivo/ranek/src/components/LoginCriar.vue
@@ -0,0 +1,62 @@
+<template>
+  <section>
+    <h2>Crie a Sua Conta</h2>
+    <ErroNotificacao :erros="erros"/>
+    <transition mode="out-in">
+      <button v-if="!criar" class="btn" @click="criar = true">Criar Conta</button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form" @click.prevent="criarUsuario">Criar Usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
+</template>
+
+<script>
+import UsuarioForm from "@/components/UsuarioForm.vue";
+
+export default {
+  name: "LoginCriar",
+  components: {
+    UsuarioForm
+  },
+  data() {
+    return {
+      criar: false,
+      erros: []
+    };
+  },
+  methods: {
+    async criarUsuario() {
+      this.erros = [];
+      try {
+        await this.$store.dispatch("criarUsuario", this.$store.state.usuario);
+        await this.$store.dispatch("logarUsuario", this.$store.state.usuario);
+        await this.$store.dispatch(
+          "getUsuario");
+        this.$router.push({ name: "usuario" });
+      } catch (error) {
+        this.erros.push(error.response.data.message);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+</style>
